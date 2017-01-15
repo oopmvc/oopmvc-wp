f26c9bcfdc3c6986a2db80a1aae4c13511c6f1f3
Shahinul Islam <shahinul.islam@cloudbeds.com>
2017-01-15 16:14:31 +0600

    remove video player js

--- training.php
+++ training.php
@@ -1,9 +1,38 @@
 <?php 
  
 
-function oopmvc_customposts_func($data){
-  return var_export($data);
-}
+// function oopmvc_recordtraining_func($data){
+      
+//       $post   = get_post( $data['id'] );
+       
+//       if ( !is_user_logged_in() ) {
+//         return new WP_Error( 'user_not_logged_in', 'You need to logged in to check Training', array( 'status' => 201 ) );
+//       }
+
+//       if ( empty( $posts ) ) {
+//         return new WP_Error( 'post_not_found', 'Training not found', array( 'status' => 404 ) );
+//       }
+
+//       $response = new WP_REST_Response( $post );
+
+//       // Add a custom status code
+//       $response->set_status( 200 );
+  
+//       return $response;
+
+// }
+// add_action( 'rest_api_init', function () {
+//   register_rest_route( 'oopmvc/v1', '/recordtraining/(?P<id>\d+)', array(
+//     'methods' => 'GET',
+//     'callback' => 'oopmvc_recordtraining_func',
+//   ) );
+// } );
+
+ 
+
+
+
+
 
 
 function filter_training_json( $data, $post, $context ) {
@@ -20,16 +49,15 @@ function filter_training_json( $data, $post, $context ) {
     }
 	
 
-    $taxonomies = wp_get_post_terms($post->ID, 'categoriestraining');
+    $taxonomies = wp_get_post_terms($post->ID, 'training_categories');
     $taxonomies_slug = array();
-    foreach($taxonomies as $tax){
+    foreach($taxonomies as $tax){ 
 		$taxonomies_slug[] = $tax->slug;
     }
 
-	$data->data['taxonomy']  = implode($taxonomies_slug, ' ');
+	 $data->data['taxonomy']  = implode($taxonomies_slug, ' ');
  
-    
- 
+     
 
     return $data;
 }
